8a485d6140b1cd6d7e88502f6619b68fa4dd6e9f
le-garff-yoann <pe.weeble@yahoo.fr>
2016-08-13 00:06:54 +0200

    move JSON::Validator things out of Navel::Base::Definition in daughters classes

--- lib/Navel/Base/Definition.pm
+++ lib/Navel/Base/Definition.pm
@@ -9,31 +9,13 @@ package Navel::Base::Definition 0.1;
 
 use Navel::Base;
 
-use JSON::Validator;
-
 use Navel::Utils qw/
     clone
+    blessed
     croak
-    try_require_namespace
 /;
 
 #-> methods
-
-sub _prepare_definition {
-    my ($class, $swagger_loader_class, $swagger_definition_path) = @_;
-
-    my $error = try_require_namespace($swagger_loader_class);
-
-    croak($error) if $error;
-
-    no strict 'refs';
-
-    $class = ref $class || $class;
-
-    *{$class . '::swagger_definition'} = sub {
-        state $definition = $swagger_loader_class->new()->expand()->api_spec()->get($swagger_definition_path);
-    };
-}
 
 sub _create_setters {
     my $class = shift;
@@ -44,7 +26,7 @@ sub _create_setters {
 
     for my $property (@_) {
         *{$class . '::set_' . $property} = sub {
-            shift->SUPER::merge(
+            shift->merge(
                 {
                     $property => shift
                 }
@@ -76,27 +58,23 @@ sub validate {
 
     my $definition_fullname;
 
-    my @errors = JSON::Validator->new()->schema($options{validator})->validate($options{raw_definition});
-
-    push @errors, @{$options{code_validator}->()} if ref $options{code_validator} eq 'CODE';
+    push my @errors, @{$options{validator}->($options{raw_definition})} if ref $options{validator} eq 'CODE';
 
     if (defined $options{if_possible_suffix_errors_with_key_value}) {
         local $@;
 
-        my $definition_name = eval {
-            $options{raw_definition}->{$options{if_possible_suffix_errors_with_key_value}};
-        };
-
-        $definition_fullname = $definition_name if defined $definition_name;
-
-        $definition_fullname = $options{definition_class} . '[' . $definition_fullname . ']';
+        $definition_fullname = $options{definition_class} . '[' . (
+            eval {
+                $options{raw_definition}->{$options{if_possible_suffix_errors_with_key_value}};
+            } // ''
+        ) . ']';
     } else {
         $definition_fullname = $options{definition_class};
     }
 
     [
         map {
-            $definition_fullname . ': ' . (defined $_  ? $_ : '?')
+            $definition_fullname . ': ' . ($_ // '?')
         } @errors
     ];
 }
