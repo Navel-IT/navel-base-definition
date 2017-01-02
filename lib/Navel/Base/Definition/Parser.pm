0507821f7ad462d7dbf8e296d118be3eb113a1ef
le-garff-yoann <pe.weeble@yahoo.fr>
2017-01-02 17:52:25 +0100

    update copyright interval: 2015-2016 -> 2015-2017

--- lib/Navel/Base/Definition/Parser.pm
+++ lib/Navel/Base/Definition/Parser.pm
@@ -1,4 +1,4 @@
-# Copyright (C) 2015-2016 Yoann Le Garff, Nicolas Boquet and Yann Le Bras
+# Copyright (C) 2015-2017 Yoann Le Garff, Nicolas Boquet and Yann Le Bras
 # navel-base-definition is licensed under the Apache License, Version 2.0
 
 #-> BEGIN
@@ -190,7 +190,7 @@ Navel::Base::Definition::Parser
 
 =head1 COPYRIGHT
 
-Copyright (C) 2015-2016 Yoann Le Garff, Nicolas Boquet and Yann Le Bras
+Copyright (C) 2015-2017 Yoann Le Garff, Nicolas Boquet and Yann Le Bras
 
 =head1 LICENSE
 
